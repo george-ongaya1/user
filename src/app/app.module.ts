6da9c1cbc04af77690910f4921dba02c0361652e
George-ongaya1git config --global user.email georgeongaya94.gmail.comgit config --golabl user.name George-ongaya1 <georgeongaya94@gmail.com>
2021-08-02 10:27:46 +0300

    added import declarations in app modue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RepositoryComponent } from './repository/repository.component';
-
+import { Ng2OrderModule } from 'ng2-order-pipe';
+import { Ng2OrderPipe } from 'ng2-order-pipe';
+import { NgxPaginationModule } from 'ngx-pagination';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,10 @@ import { RepositoryComponent } from './repository/repository.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Ng2OrderModule,
+    NgxPaginationModule,
+    Ng2OrderPipe
   ],
   providers: [],
   bootstrap: [AppComponent]
